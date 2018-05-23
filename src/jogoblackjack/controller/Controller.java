3e2443030d74341fdeaa7d95577c355dfe1eaccb
danco <danco@dandan>
2018-05-22 22:01:42 -0300

    alteração

--- src/jogoblackjack/controller/Controller.java
+++ src/jogoblackjack/controller/Controller.java
@@ -23,10 +23,12 @@ public class Controller {
 
     }
 
+    //adiciona jogador na lista encadeada
     public void addJogador(Jogador jogador) {
         this.jogadores.addLast(jogador);
     }
 
+    //inicia uma partida fazendo o usuario escolher a quantidade de jogadores
     public void iniciarPartida() {
         int numDeJogadores;
         System.out.println("Digite a quantidade de jogadores:");
@@ -34,16 +36,17 @@ public class Controller {
         while (numDeJogadores > 5) {
             System.out.println("Número de jogadores acima do limite");
             numDeJogadores = Integer.parseInt(scan.nextLine());
-
         }
-       partida.addJogadorNaPartida(numDeJogadores, jogadores);
+       partida.addJogadorNaPartida(numDeJogadores, jogadores);//chama metodo para adicionar jogador na partida
        partida.pegarCarta();
     }
 
+    //cria um baralho, embaralha e coloca dentro de uma variavel
     public void criarBaralho() {
         cartas = baralho.embaralharEAddPilha(baralho.addCartas());
     }
 
+    //metodo para distribuir as cartas
     public Object darCartas() {
         return cartas.pop();
     }
--- src/jogoblackjack/model/Croupier.java
+++ src/jogoblackjack/model/Croupier.java
@@ -13,15 +13,22 @@ public class Croupier extends Jogador {
         listadecartas = new LinkedList();
     }
 
+    //retorna a carta que o croupier acabou de pegar
     @Override
-    public int pegarCarta(Carta carta) {
+    public Object pegarCarta(Carta carta) {
         this.listadecartas.addLast(carta);
+        return carta;
+    }
+
+    //retorna os pontos do jogador
+    @Override
+     public int cartasNaMao(){
         return maodecarta.CartasNaMao(listadecartas);
     }
     
+    //lista de todas as cartas do croupier
     @Override
-     public Ilist cartasNaMao(){
+    public Ilist cartas(){
         return this.listadecartas;
-    }//alterar para só aparecer uma carta
-
+    } 
 }
