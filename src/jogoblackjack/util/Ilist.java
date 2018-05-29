c4d0b18a09907f78654df93e295ab87565a6349f
danco <danco@dandan>
2018-05-29 02:17:46 -0300

    Revert "Revert "JAVADOC""
    This reverts commit 28b5820ec3be63c303d1b248365b8533083d0717.

--- src/jogoblackjack/util/Ilist.java
+++ src/jogoblackjack/util/Ilist.java
@@ -1,19 +1,58 @@
 package jogoblackjack.util;
 
-public interface Ilist{
-    
+/**
+ *
+ * @author Daniel Alves e Gabriela dos Santos
+ */
+public interface Ilist {
+
+    /**
+     * Método para adicionar no início da lista
+     *
+     * @param data - objeto que irá ser adicionado
+     */
     public void addFirst(Object data);
-    
+
+    /**
+     * Método para adicionar no final da lista
+     *
+     * @param data - objeto que irá ser adicionado
+     */
     public void addLast(Object data);
-    
+
+    /**
+     * Método para remover no início da lista
+     *
+     * @return objeto removido
+     */
     public Object removeFirst();
-    
-    public Object removeLast();    
- 
+
+    /**
+     * Método para remover no final da lista
+     *
+     * @return objeto removido
+     */
+    public Object removeLast();
+
+    /**
+     * Método para saber o tamanho da lista
+     *
+     * @return o tamanho da lista
+     */
     public int size();
-    
+
+    /**
+     * Método para saber se a lista está vazia
+     *
+     * @return true caso a lista esteja vazia
+     */
     public boolean isEmpty();
-    
+
+    /**
+     * Método do iterator
+     *
+     * @return o iterator da lista
+     */
     public Iterator iterator();
-    
-} 
+
+}
