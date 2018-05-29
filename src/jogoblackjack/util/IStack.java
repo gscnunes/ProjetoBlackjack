58f0303b063be2b89a3931200745544b29f68138
danco <danco@dandan>
2018-05-29 01:59:33 -0300

    JAVADOC

--- src/jogoblackjack/util/IStack.java
+++ src/jogoblackjack/util/IStack.java
@@ -1,12 +1,36 @@
 package jogoblackjack.util;
 
+/**
+ *
+ * @author Daniel Alves e Gabriela dos Santos
+ */
 public interface IStack {
 
+    /**
+     * Método para adicionar um objeto na pilha
+     *
+     * @param data - objeto que irá ser adicionado
+     */
     public void push(Object data);
 
+    /**
+     * Método para remover o último objeto adicionado da pilha
+     *
+     * @return objeto removido
+     */
     public Object pop();
 
+    /**
+     * Método para mostrar o último objeto adicionado
+     *
+     * @return objeto que está na "cabeça" da pilha
+     */
     public Object peek();
 
+    /**
+     * Método para saber se a pilha está vazia
+     *
+     * @return true caso a pilha esteja vazia
+     */
     public boolean isEmpty();
 }
--- src/jogoblackjack/util/Iterator.java
+++ src/jogoblackjack/util/Iterator.java
@@ -1,9 +1,23 @@
 package jogoblackjack.util;
 
-public interface Iterator{
-    
+/**
+ *
+ * @author Daniel Alves e Gabriela dos Santos
+ */
+public interface Iterator {
+
+    /**
+     * Método para saber se tem um próximo elemento na lista
+     *
+     * @return true caso tenha elemento
+     */
     public boolean hasNext();
-    
+
+    /**
+     * Método para retornar o elemento da lista
+     *
+     * @return elemento da lista
+     */
     public Object next();
-    
+
 }
