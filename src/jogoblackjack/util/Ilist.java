28b5820ec3be63c303d1b248365b8533083d0717
danco <danco@dandan>
2018-05-29 02:16:58 -0300

    Revert "JAVADOC"
    This reverts commit 58f0303b063be2b89a3931200745544b29f68138.

--- src/jogoblackjack/util/Ilist.java
+++ src/jogoblackjack/util/Ilist.java
@@ -1,58 +1,19 @@
 package jogoblackjack.util;
 
-/**
- *
- * @author Daniel Alves e Gabriela dos Santos
- */
-public interface Ilist {
-
-    /**
-     * Método para adicionar no início da lista
-     *
-     * @param data - objeto que irá ser adicionado
-     */
+public interface Ilist{
+    
     public void addFirst(Object data);
-
-    /**
-     * Método para adicionar no final da lista
-     *
-     * @param data - objeto que irá ser adicionado
-     */
+    
     public void addLast(Object data);
-
-    /**
-     * Método para remover no início da lista
-     *
-     * @return objeto removido
-     */
+    
     public Object removeFirst();
-
-    /**
-     * Método para remover no final da lista
-     *
-     * @return objeto removido
-     */
-    public Object removeLast();
-
-    /**
-     * Método para saber o tamanho da lista
-     *
-     * @return o tamanho da lista
-     */
+    
+    public Object removeLast();    
+ 
     public int size();
-
-    /**
-     * Método para saber se a lista está vazia
-     *
-     * @return true caso a lista esteja vazia
-     */
+    
     public boolean isEmpty();
-
-    /**
-     * Método do iterator
-     *
-     * @return o iterator da lista
-     */
+    
     public Iterator iterator();
-
-}
+    
+} 
--- src/jogoblackjack/util/Iterator.java
+++ src/jogoblackjack/util/Iterator.java
@@ -1,23 +1,9 @@
 package jogoblackjack.util;
 
-/**
- *
- * @author Daniel Alves e Gabriela dos Santos
- */
-public interface Iterator {
-
-    /**
-     * Método para saber se tem um próximo elemento na lista
-     *
-     * @return true caso tenha elemento
-     */
+public interface Iterator{
+    
     public boolean hasNext();
-
-    /**
-     * Método para retornar o elemento da lista
-     *
-     * @return elemento da lista
-     */
+    
     public Object next();
-
+    
 }
